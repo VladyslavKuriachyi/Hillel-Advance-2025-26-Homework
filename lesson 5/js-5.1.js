8a374def448d8231f656c1daa1619fe2955c6ebc
Vladyslav Kuriachyi <vladlucky0@gmail.com>
2025-09-08 21:50:56 +0100

    6.2 21.50 (08/08/2025)

--- lesson 5/js-5.1.js
+++ lesson 5/js-5.1.js
@@ -1,12 +1,11 @@
-let result= "";
-for (let i = 20; i<=30; i+=0.5) {
-    result += i+ " ";
+let result = "";
+for (let i = 20; i <= 30; i += 0.5) {
+    result += i + " ";
     console.log(result.trim());
 }
 
 
-
-const rate = 26 ;
- for (let dollars=10;  dollars <= 100; dollars = dollars + 10) {
-     console.log ( `${dollars} USD = ${dollars * rate} UAH`);
- }
+const rate = 26;
+for (let dollars = 10; dollars <= 100; dollars = dollars + 10) {
+    console.log(`${dollars} USD = ${dollars * rate} UAH`);
+}
