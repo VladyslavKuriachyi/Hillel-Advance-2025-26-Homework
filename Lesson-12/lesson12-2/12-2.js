6c75d76355d918dde0acea891a8f2d139cc2e73b
Vladyslav Kuriachyi <vladlucky0@gmail.com>
2025-10-05 19:50:26 +0100

    12.2 html/css/js 19.50

--- Lesson-12/lesson12-2/12-2.js
+++ Lesson-12/lesson12-2/12-2.js
@@ -1,13 +1,11 @@
 const container = document.getElementById('container');
 
-container.addEventListener('click',function(event) {
+container.addEventListener('click', function (event) {
     if (event.target.classList.contains('child-1')) {
         alert('Button 1 clicked');
-    }
-    else if (event.target.classList.contains('child-2')) {
+    } else if (event.target.classList.contains('child-2')) {
         alert('Button 2 clicked');
-    }
-    else {
+    } else {
         alert('Button 3 clicked');
     }
 
